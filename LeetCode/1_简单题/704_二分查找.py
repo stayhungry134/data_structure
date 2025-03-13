4244c55d07cdddae593495b21fb8244118aeee34
Ethan <stayhungry134@gmail.com>
2025-03-13 15:35:33 +0800

    feat: 重构二分查找为类方法

--- LeetCode/1_简单题/704_二分查找.py
+++ LeetCode/1_简单题/704_二分查找.py
@@ -6,21 +6,27 @@ author: Ethan
 Description: 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
 """
+from re import search
+from typing import List
 
 
-def search(nums: list[int], target: int) -> int:
-    left = 0
-    right = len(nums) - 1
-    while left <= right:
-        mid = (right + left) >> 1
-        if target < nums[mid]:
-            right = mid - 1
-        elif target > nums[mid]:
-            left = mid + 1
-        else:
-            return mid
-    return -1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        right = len(nums) - 1
+        left = 0
+        while left <= right:
+            mid = left + (right - left) // 2
 
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                right = mid - 1
+            if target > nums[mid]:
+                left = mid + 1
+        return -1
 
-nums = [-1, 0, 3, 5, 9, 12]
-print(search(nums, 13))
+solution = Solution()
+nums = [-1,0,3,5,9,12]
+target = 9
+
+print(solution.search(nums, target))
