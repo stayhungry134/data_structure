5e5bb8816914cdd57e57ac352a2f6dfa1283ed4a
Ethan <stayhungry134@gmail.com>
2025-03-13 15:53:25 +0800

    feat: 添加类方法实现回文数判断

--- LeetCode/1_简单题/09_回文数.py
+++ LeetCode/1_简单题/09_回文数.py
@@ -32,6 +32,13 @@ def is_palindrome1(x: int) -> bool:
         x = x // 10
     return original_x == reverse_x
 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+        for i in range(len(x_str) // 2):
+            if x_str[i] != x_str[-i-1]:
+                return False
+        return True
 
-num = 121
-print(is_palindrome1(num))
+num = 12334321
+print(Solution().isPalindrome(num))
