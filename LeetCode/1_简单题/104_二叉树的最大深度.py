a848a0af4acdf3478d3966ad96adf394cc7e0937
Ethan <stayhungry134@gmail.com>
2025-06-10 22:56:52 +0800

    feat: 实现104_二叉树的最大深度算法

--- LeetCode/1_简单题/104_二叉树的最大深度.py
+++ LeetCode/1_简单题/104_二叉树的最大深度.py
@@ -20,4 +20,11 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        pass
+        def dfs(node: Optional[TreeNode]):
+            if node is None:
+                return 0
+            else:
+                left_depth = dfs(node.left)
+                right_depth = dfs(node.right)
+                return 1 + max(left_depth, right_depth)
+        return dfs(root)
