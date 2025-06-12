7b184c7a8fc09bcca227ca468942fd095c42b21b
Ethan <stayhungry134@gmail.com>
2025-06-12 17:59:22 +0800

    feat: 优化1869_哪种连续子字符串更长算法实现，简化逻辑和变量使用

--- LeetCode/1_简单题/1869_哪种连续子字符串更长.py
+++ LeetCode/1_简单题/1869_哪种连续子字符串更长.py
@@ -9,21 +9,28 @@ Description: https://leetcode.cn/problems/longer-contiguous-segments-of-ones-tha
 
 class Solution:
     def checkZeroOnes(self, s: str) -> bool:
-        res_map = {
-            0: 0,
-            1: 0,
-        }
-        tem_map = {
-            0: 0,
-            1: 0,
-        }
+        n = len(s)
+        len_0 = 1 if s[0] == '0' else 0
+        len_1 = 1 if s[0] == '1' else 0
+        tem_cnt = 0
+        pre_c = ''
         for c in s:
-            if c == '1':
-                tem_map[1] += 1
-                tem_map[0] = 0
-                res_map[1] = max(tem_map[1], res_map[1])
+            if c == pre_c:
+                tem_cnt += 1
             else:
-                tem_map[0] += 1
-                tem_map[1] = 0
-                res_map[0] = max(tem_map[0], res_map[0])
-        return res_map[1] > res_map[0]
+                if pre_c == '1':
+                    len_1 = max(len_1, tem_cnt)
+                else:
+                    len_0 = max(len_0, tem_cnt)
+                tem_cnt = 1
+            pre_c = c
+        if pre_c == '1':
+            len_1 = max(tem_cnt, len_1)
+        else:
+            len_0 = max(tem_cnt, len_0)
+        return len_1 > len_0
+
+
+s = "0"
+
+print(Solution().checkZeroOnes(s))
