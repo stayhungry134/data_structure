905e8a0b4ad525e0790479035b0d6f28c36c3d67
Ethan <stayhungry134@gmail.com>
2025-06-16 17:48:42 +0800

    feat: 添加新的方法以统计前后缀下标对

--- LeetCode/1_简单题/3042_统计前后缀下标对I.py
+++ LeetCode/1_简单题/3042_统计前后缀下标对I.py
@@ -14,6 +14,21 @@ class Solution:
             for j, word2 in enumerate(words[i+1:]):
                 if word2.startswith(word1) and word2.endswith(word1):
                     res += 1
+                else:
+                    continue
+        return res
+
+    def countPrefixSuffixPairs1(self, words: List[str]) -> int:
+        from collections import defaultdict
+
+        tem_map = defaultdict(int)
+        res = 0
+
+        for word in words:
+            for prefix in tem_map:
+                if word.startswith(prefix) and word.endswith(prefix):
+                    res += tem_map[prefix]
+            tem_map[word] += 1
         return res
 
 
