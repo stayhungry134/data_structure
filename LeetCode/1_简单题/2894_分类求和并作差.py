957ddcad4f2848c3ed2dc79faec0218d5e2dbbcc
Ethan <stayhungry134@gmail.com>
2025-06-17 11:58:31 +0800

    feat: 添加新的方法以使用数学推导计算分类求和的差值

--- LeetCode/1_简单题/2894_分类求和并作差.py
+++ LeetCode/1_简单题/2894_分类求和并作差.py
@@ -17,3 +17,11 @@ class Solution:
                 ls1.append(i)
 
         return sum(ls1) - sum(ls2)
+
+    def differenceOfSums2(self, n: int, m: int) -> int:
+        # 使用数学推导
+        # num2 = m + m*2+ ... + m*k
+        # num1 = 1 + 2 + ... + n - num2
+        # res就等于 1加到n 减去两倍的 num2
+        k = n // m
+        return n * (n + 1) // 2 - k * (k + 1) * m
