fe72f5cdc7f890c2937b6bf9229378a11684bcf4
Ethan <stayhungry134@gmail.com>
2025-07-07 22:42:55 +0800

    feat: 添加1461_检查一个字符串是否包含所有长度为 K 的二进制子串算法实现

---
+++ LeetCode/2_中等难度/1461_检查一个字符串是否包含所有长度为 K 的二进制子串.py
@@ -0,0 +1,21 @@
+"""
+name: 1461_检查一个字符串是否包含所有长度为 K 的二进制子串.py
+create_time: 2025/7/7 22:25
+author: Ethan
+
+Description: https://leetcode.cn/problems/check-if-a-string-contains-all-binary-codes-of-size-k/description/
+"""
+
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        tem_set = set()
+        for i in range(len(s) - k + 1):
+            tem_s = s[i: i+k]
+            tem_set.add(tem_s)
+        print(tem_set)
+        return len(tem_set) >= 1 << k
+
+s = "00110"
+k = 2
+print(Solution().hasAllCodes(s, k))
